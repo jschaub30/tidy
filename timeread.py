60f8feff2b772bb2871973746f665c68883131ad
Jeremy Schaub <schaubjeremy@gmail.com>
2015-07-06 17:17:46 -0500

    Script continues when problem with input file

--- timeread.py
+++ timeread.py
@@ -14,14 +14,14 @@ import sys
 class Measurement:
 
     '''
-    Data structure for pwatch measurement
+    Data structure for /usr/bin/time measurement
     '''
 
     def __init__(self):
         self.elapsed_time_sec = ''
         self.user_time_sec = ''
         self.system_time_sec = ''
-        self.exit_status = ''
+        self.exit_status = '-1'
         self.cpu_pct = ''
 
     def fields(self):
@@ -57,38 +57,42 @@ def main(time_fn):
 
 
 def parse(time_fn):
-    m = Measurement()
     '''
     This parses the output of "/usr/bin/time --verbose"
     Parsing these fields:  exit_status, user_time_sec, elapsed_time_sec,
                            system_time_sec, cpu_percent
     '''
-    with open(time_fn, 'r') as fid:
-        blob = fid.read()
-    m.exit_status = blob.split('Exit status: ')[1].split('\n')[0].strip()
-    if m.exit_status != '0':
-        sys.stderr.write(
-            "WARNING! non-zero exit status = %s in file \n\t%s\n" %
-            (m.exit_status, time_fn))
-    m.user_time_sec = blob.split(
-        'User time (seconds): ')[1].split('\n')[0].strip()
-    m.system_time_sec = blob.split(
-        'System time (seconds): ')[1].split('\n')[0].strip()
-    val = blob.split('Elapsed (wall clock) time (h:mm:ss or m:ss): ')[
-        1].split('\n')[0].strip()
-    if len(val.split(':')) == 2:   # m:ss
-        val = str(
-            int(val.split(':')[0]) * 60 + float(val.split(':')[1].strip()))
-    elif len(val.split(':')) == 3:   # h:m:ss
-        val = str(int(val.split(':')[
-                  0]) * 3600 + int(val.split(':')[1]) * 60 +
-                  float(val.split(':')[2].strip()))
-    m.elapsed_time_sec = val
-    m.cpu_pct = blob.split('Percent of CPU this job got: ')[
-        1].split('\n')[0].strip('%')
-    if not m.is_valid():
-        sys.stderr.write('Problem parsing file %s\n' % time_fn)
-        assert False
+    m = Measurement()
+    try:
+        with open(time_fn, 'r') as fid:
+            blob = fid.read()
+        m.exit_status = blob.split('Exit status: ')[1].split('\n')[0].strip()
+        if m.exit_status != '0':
+            sys.stderr.write(
+                "WARNING! non-zero exit status = %s in file \n\t%s\n" %
+                (m.exit_status, time_fn))
+        m.user_time_sec = blob.split(
+            'User time (seconds): ')[1].split('\n')[0].strip()
+        m.system_time_sec = blob.split(
+            'System time (seconds): ')[1].split('\n')[0].strip()
+        val = blob.split('Elapsed (wall clock) time (h:mm:ss or m:ss): ')[
+            1].split('\n')[0].strip()
+        if len(val.split(':')) == 2:   # m:ss
+            val = str(
+                int(val.split(':')[0]) * 60 + float(val.split(':')[1].strip()))
+        elif len(val.split(':')) == 3:   # h:m:ss
+            val = str(int(val.split(':')[
+                      0]) * 3600 + int(val.split(':')[1]) * 60 +
+                      float(val.split(':')[2].strip()))
+        m.elapsed_time_sec = val
+        m.cpu_pct = blob.split('Percent of CPU this job got: ')[
+            1].split('\n')[0].strip('%')
+        if not m.is_valid():
+            sys.stderr.write('Problem parsing file %s\n' % time_fn)
+            assert False
+    except:
+        sys.stderr.write('Problem parsing time file %s\n' % time_fn)
+        pass
     return m
 
 if __name__ == '__main__':
